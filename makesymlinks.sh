438c5c9a99d9729941632cca009829276ed70a34
sandos187 <sandos@mailbox.org>
2016-03-16 13:02:10 +0100

    added curlrc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,13 +4,13 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
+########### Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc curlrc vimrc vim zshrc oh-my-zsh private sshrc sshrc.d"    # list of files/folders to symlink in homedir
 
-##########
+###########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
